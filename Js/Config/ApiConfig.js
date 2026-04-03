99a0c29a2eaf54bcba717bdbbc7576c3073f3a16
ejimasyota <ejimasyota0902@icloud.com>
2026-04-03 04:05:35 +0000

    APIキー

---
+++ Js/Config/ApiConfig.js
@@ -0,0 +1,22 @@
+/**
+ * API設定定数
+ */
+const GOOGLE_MAPS_API_KEY = "AIzaSyDv0HsFREf3O--TufqQCxxzSDu5iOqfEOY";
+
+const HOTPEPPER_API_KEY = "38c52fa7d3b8941b";
+
+const HOTPEPPER_BASE_URL = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
+
+const HOTPEPPER_GENRE_URL = "https://webservice.recruit.co.jp/hotpepper/genre/v1/";
+
+const MLIT_API_KEY = "Pq..mGHxR-7m6.L6vaF3rxKT1RDZgjVF";
+
+const MLIT_BASE_URL = "https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001";
+
+const KAGOSHIMA_PREF_CODE = "46";
+
+const SEARCH_RESULTS_PER_PAGE = 10;
+
+const RECOMMEND_DISPLAY_COUNT = 10;
+
+const HOTPEPPER_LARGE_AREA = "SA46";
